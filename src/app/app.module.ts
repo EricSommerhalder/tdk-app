938b68770f6e20d3987ba0ac8e66d2cda0be95da
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2020-10-28 15:18:41 +0100

    New set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  AppInitService,
+  DspApiConfigToken,
+  DspApiConnectionToken,
+  DspActionModule,
+  DspCoreModule,
+  DspSearchModule,
+  DspViewerModule
+} from '@dasch-swiss/dsp-ui';
+import {environment} from '../environments/environment';
+import {KnoraApiConnection} from '@dasch-swiss/dsp-js';
+import { PlaygroundComponent } from './playground/playground.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DspCoreModule,
+    DspViewerModule,
+    DspSearchModule,
+    DspActionModule,
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER, // see https://angular.io/api/core/APP_INITIALIZER
+      useFactory: (appInitService: AppInitService) =>
+        (): Promise<any> => {
+          return appInitService.Init('config', environment);
+        },
+      deps: [AppInitService], // depends on AppInitService
+      multi: true
+    },
+    {
+      provide: DspApiConfigToken,
+      useFactory: (appInitService: AppInitService) => appInitService.dspApiConfig, // AppInitService is passed to the factory method
+      deps: [AppInitService] // depends on AppInitService
+    },
+    {
+      provide: DspApiConnectionToken,
+      useFactory: (appInitService: AppInitService) => new KnoraApiConnection(appInitService.dspApiConfig), // AppInitService is passed to the factory method
+      deps: [AppInitService] // depends on AppInitService
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  name: 'Tal der Könige',
   production: false
 };
 
